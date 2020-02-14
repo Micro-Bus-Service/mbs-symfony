3614147747222a58e332d6d17cefb34648ba455d
cyril.poidevin <cyril.poidevin@multi-impact.com>
2020-02-14 13:39:06 +0100

    fix: Tests

--- Tests/DependencyInjection/ConfigurationTest.php
+++ Tests/DependencyInjection/ConfigurationTest.php
@@ -2,52 +2,21 @@
 
 namespace Mbs\MbsBundle\Tests\DependencyInjection;
 
-use Mbs\MbsBundle\DependencyInjection\MbsExtension;
-use Mbs\MbsBundle\MbsBundle;
 use Mbs\MbsBundle\Tests\TestCase;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Mbs\MbsBundle\DependencyInjection\Configuration;
+use Symfony\Component\Config\Definition\Processor;
 
 class ConfigurationTest extends TestCase
 {
-    public function testLoadEmptyConfiguration()
+    public function testDoNoDuplicateDefaultFormResources()
     {
-        $container = $this->createContainer();
-        $container->registerExtension(new MbsExtension());
-        $container->loadFromExtension('mbs');
-        $this->compileContainer($container);
+        $input = [
+            'server' => ['protocol' => 'https'],
+        ];
 
-        var_dump($container->getDefinitions());
+        $processor = new Processor();
+        $config = $processor->processConfiguration(new Configuration(), [$input]);
 
-        $this->assertEquals('Mbs\Environment', $container->getDefinition('mbs')->getClass());
-    }
-
-    private function createContainer()
-    {
-        $container = new ContainerBuilder(new ParameterBag([
-            'kernel.cache_dir' => __DIR__,
-            'kernel.project_dir' => __DIR__,
-            'kernel.charset' => 'UTF-8',
-            'kernel.debug' => false,
-            'kernel.bundles' => [
-                'MbsBundle' => MbsBundle::class,
-            ],
-            'kernel.bundles_metadata' => [
-                'MbsBundle' => [
-                    'namespace' => 'Mbs\MbsBundle\DependencyInjection\MbsBundle',
-                    'path' => __DIR__ . '/MbsBundle',
-                ],
-            ],
-        ]));
-
-        return $container;
-    }
-
-    private function compileContainer(ContainerBuilder $container)
-    {
-        $container->getCompilerPassConfig()->setOptimizationPasses([]);
-        $container->getCompilerPassConfig()->setRemovingPasses([]);
-        $container->getCompilerPassConfig()->setAfterRemovingPasses([]);
-        $container->compile();
+        $this->assertEquals('https', $config['server']['protocol']);
     }
 }
--- Tests/DependencyInjection/MbsExtensionTest.php
+++ Tests/DependencyInjection/MbsExtensionTest.php
@@ -2,8 +2,50 @@
 
 namespace Mbs\MbsBundle\Tests\DependencyInjection;
 
+use Mbs\MbsBundle\DependencyInjection\MbsExtension;
+use Mbs\MbsBundle\MbsBundle;
 use Mbs\MbsBundle\Tests\TestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
 
 class MbsExtensionTest extends TestCase
 {
+    public function testLoadEmptyConfiguration()
+    {
+        $container = $this->createContainer();
+        $container->registerExtension(new MbsExtension());
+        $container->loadFromExtension('mbs');
+        $this->compileContainer($container);
+
+        $this->assertInstanceOf('Mbs\MbsBundle\DependencyInjection\MbsExtension', $container->getExtension('mbs'));
+    }
+
+    private function createContainer()
+    {
+        $container = new ContainerBuilder(new ParameterBag([
+            'kernel.cache_dir' => __DIR__,
+            'kernel.project_dir' => __DIR__,
+            'kernel.charset' => 'UTF-8',
+            'kernel.debug' => false,
+            'kernel.bundles' => [
+                'MbsBundle' => MbsBundle::class,
+            ],
+            'kernel.bundles_metadata' => [
+                'MbsBundle' => [
+                    'namespace' => 'Mbs\MbsBundle\DependencyInjection\MbsBundle',
+                    'path' => __DIR__ . '/MbsBundle',
+                ],
+            ],
+        ]));
+
+        return $container;
+    }
+
+    private function compileContainer(ContainerBuilder $container)
+    {
+        $container->getCompilerPassConfig()->setOptimizationPasses([]);
+        $container->getCompilerPassConfig()->setRemovingPasses([]);
+        $container->getCompilerPassConfig()->setAfterRemovingPasses([]);
+        $container->compile();
+    }
 }
